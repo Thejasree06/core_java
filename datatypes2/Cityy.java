bf4231836007dd2bac6f3b529135ff744bb5f06f
thejasree06 <thejasreers.xworkz@gmail.com>
2023-11-10 11:42:14 +0530

    type

---
+++ datatypes2/Cityy.java
@@ -0,0 +1,22 @@
+class Cityy{
+		
+		String name="Davanagere";
+		String statename="Karnataka";
+			
+		Cityy()
+		{
+			
+		}		
+		void show()
+		{
+			System.out.println("show information of city");
+		    System.out.println("name :"+this.name);
+			System.out.println("name :"+this.statename);
+			System.out.println("end information of city");
+			
+			
+		}
+		
+
+
+}
