757b836aec1706e0890308680f15412e54c5b349
thejasree06 <thejasreers.xworkz@gmail.com>
2023-11-21 09:38:09 +0530

    inh

---
+++ Inheritance/Develops.java
@@ -0,0 +1,11 @@
+class Develops extends Personn
+{
+	Develops()
+	{
+		System.out.println("Starting no arg in Develops");
+	}
+	void runBuild()
+	{
+		System.out.println("Starting develops in Person");
+	}
+}
