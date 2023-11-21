4a1ea0310eb445e4d7628b23ae496bbef8998fac
thejasree06 <thejasreers.xworkz@gmail.com>
2023-11-21 09:45:36 +0530

    inh

---
+++ Inheritance/ProductOwner.java
@@ -0,0 +1,11 @@
+class ProductOwner extends Personn
+{
+	ProductOwner()
+	{
+		System.out.println("Starting no arg in Business");
+	}
+	void validate()
+	{
+		System.out.println("Starting validate in ProductOwner");
+	}
+}
