6274ae3dccad43d6df22e4013b36e82a5035ecb6
thejasree06 <thejasreers.xworkz@gmail.com>
2023-11-21 09:39:29 +0530

    inh

---
+++ Inheritance/Personn.java
@@ -0,0 +1,15 @@
+class Personn
+{
+	Personn()
+	{
+		System.out.println("Starting from person");
+	}
+	void eat()
+	{
+		System.out.println("Stsrting eat in Person");
+	}
+	void sleep()
+	{
+		System.out.println("Starting code in sleep");
+	}
+}
