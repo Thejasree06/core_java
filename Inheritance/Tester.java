3596e6950666eee80bfb7a0af7145d063e60b28e
thejasree06 <thejasreers.xworkz@gmail.com>
2023-11-21 09:47:53 +0530

    inh

---
+++ Inheritance/Tester.java
@@ -0,0 +1,11 @@
+class Tester extends Developer
+{
+	Tester()
+	{
+		System.out.println("Starting no arg in Tester");
+	}
+	void qualityCheck()
+	{
+		System.out.println("Starting quality check in tester");
+	}
+}
