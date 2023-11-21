378e1b260b31adfd1f934de6e6dbca61857cc10e
thejasree06 <thejasreers.xworkz@gmail.com>
2023-11-21 09:33:16 +0530

    inh

---
+++ Inheritance/DataBaseDeveloper.java
@@ -0,0 +1,11 @@
+class DataBaseDeveloper extends Developer
+{
+	DataBaseDeveloper()
+	{
+		System.out.println("Starting no arg in DataBaseDeveloper");
+	}
+	void runScript()
+	{
+		System.out.println("Stsrting runScript in Developer");
+	}
+}
