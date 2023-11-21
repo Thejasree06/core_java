e638c8634222ede80743415c93ad3571315b96ab
thejasree06 <thejasreers.xworkz@gmail.com>
2023-11-21 09:30:37 +0530

    inh

---
+++ Inheritance/BusinessAnalyst.java
@@ -0,0 +1,11 @@
+class BusinessAnalyst extends Personn
+{
+	BusinessAnalyst()
+	{
+		System.out.println("Starting no argu in BusinessAnalytics");
+	}
+	void checkRequirement()
+	{
+		System.out.println("Starting checkRequrement in BusinessAnalytics");
+	}
+}
