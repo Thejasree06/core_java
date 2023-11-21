3143aeb0251176fd6b22ed22e70d0abcbbe39159
thejasree06 <thejasreers.xworkz@gmail.com>
2023-11-21 09:35:16 +0530

    inh

---
+++ Inheritance/Developer.java
@@ -0,0 +1,12 @@
+class Developer extends Personn
+{
+	Developer()
+	{
+		System.out.println("Starting no arg const in Develper");
+	}
+	void code()
+	{
+		System.out.println("Starting code in developer");
+	}
+}
+
