ba251e1b18559b536cdfddc2fbf80dc7ff337817
thejasree06 <thejasreers.xworkz@gmail.com>
2023-11-23 00:22:45 +0530

    key

---
+++ override/FoodItem.java
@@ -0,0 +1,75 @@
+class FoodItem
+{
+	String name;
+	String veg;
+	String nonVeg;
+	boolean quality;
+	int cost;
+	double heat;
+	int no;
+	String items;
+	String sweets;
+	double weight;
+	
+	FoodItem(String name,String veg,String nonVeg,boolean quality,int cost,double heat,int no,String items,String sweets,double weight)
+	{
+		this.name=name;
+		this.veg=veg;
+		this.nonVeg=nonVeg;
+		this.quality=quality;
+		this.cost=cost;
+		this.heat=heat;
+		this.no=no;
+		this.items=items;
+		this.sweets=sweets;
+		this.weight=weight;
+	}
+	void spicy()
+	{
+		System.out.println("Starting no arg in spicy");
+	}
+	void rice()
+	{
+		System.out.println("Starting no arg in rice");
+	}
+	void dryfood()
+	{
+		System.out.println("Starting no arg in dryfood");
+	}
+	
+	void jamun()
+	{
+		System.out.println("Starting no arg in jamun");
+	}
+	void cake()
+	{
+		System.out.println("Starting no arg in cake");
+	}
+	void salty()
+	{
+		System.out.println("Starting no arg in salty");
+	}
+	void sugar()
+	{
+		System.out.println("Starting no arg in sugar");
+	}
+	void cold()
+	{
+		System.out.println("Starting no arg in cold");
+	}
+	void oil()
+	{
+		System.out.println("Starting no arg in oil");
+	}
+	void packing()
+	{
+		System.out.println("Starting no arg in packing");
+	}
+	void mixing()
+	{
+		System.out.println("Starting no arg in mixing");
+	}
+	
+	
+	
+} 
